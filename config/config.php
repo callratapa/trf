ef8c7398a0b486b160ad1c9e857722b8c02a8b99
callratapa <110785327+callratapa@users.noreply.github.com>
2022-08-07 19:59:47 +0100

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php  
+// ================================================================= //
+// ================================================================= //
+// =============== By MCh CPA 2021 : Re-skinning GRP =============== //
+// ================================================================= //
+// ================================================================= //
+
+$uri = 'https://www.re-skinning.com/insta';
+$wname = 'Free Instagram Followers'; 
+$descrip = 'Free Ig Followers : Get free Instagram followers / get more instagram followers.';  
+
+$cpa_link = 'https://cpalink.com';
+
+?>
